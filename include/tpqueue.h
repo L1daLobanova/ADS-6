032731b2f8824d20b99a0b1b40ea352ad2e7f98c
L1daLobanova <124993574+L1daLobanova@users.noreply.github.com>
2023-04-16 10:28:58 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,11 +5,11 @@
 
 template<typename T, int size>
 class TPQueue {
-  T arr[size];
+  T* arr;
   int first, last, count;
 
  public:
-  TPQueue(): first(0), last(0), count(0) {}
+  TPQueue(): arr(new T[size]), first(0), last(0), count(0) {}
   void push(const T & value) {
     if (count == size)
       throw std::string("FULL!");
